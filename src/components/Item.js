c1d199abe7de5f3060a861081bb0f2f707e40dd6
Adrian <adrianic92@yahoo.com>
2022-09-20 14:08:54 -0700

    commit tree 2f101dace6a088b73f1f0b8ea511981e309d499b

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [classy, setClass] = useState(false);
+  const cartClass = classy? "in-cart" : ""
+  const inCart = classy? "Remove From Cart" : "Add to Cart"
+  const addRem = classy? "remove" : "add"
+
+  function handleClick() {
+    setClass(change => !change)
+  }
+
   return (
-    <li className="">
+    <li className={cartClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={addRem} onClick={handleClick}>{inCart}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,22 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [filterBy, setFilterBy] = useState("All")
+
+  const itemsToDisplay = items.filter((item) => {
+    if (filterBy === "All") {
+      return true;
+    }
+    else {
+      return item.category === filterBy;
+    }
+  })
+
+
+  const itemList = itemsToDisplay.map((item) => (
+    <Item key={item.id} name={item.name} category={item.category} />
+  ))
+  
+  function handleFilterChange(e) {
+    setFilterBy(e.target.value)
+  }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleFilterChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </div>
-      <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
-      </ul>
+      <ul className="Items">{itemList}</ul>
     </div>
   );
 }
